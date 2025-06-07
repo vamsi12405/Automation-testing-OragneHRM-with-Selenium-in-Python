e4438379c1043a219186790dd981017c424a19b9
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-07 11:51:43 +0530

    Add files via upload

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@pytest.fixture
+def driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")  # or just "--headless"
+    options.add_argument("--window-size=1920,1080")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.implicitly_wait(10)
+    yield driver
+    driver.quit()
