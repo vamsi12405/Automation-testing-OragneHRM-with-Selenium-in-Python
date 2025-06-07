0a0dea8b77dd20009bbb1ebb2b5ed7d725980ee0
vamsi12405 <72076323+vamsi12405@users.noreply.github.com>
2025-06-07 11:53:27 +0530

    Delete docs directory

--- docs/leave_page.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-from .base_page import BasePage
-
-class LeavePage(BasePage):
-    HEADER = (By.XPATH, "//h6[text()='Leave']")
-    def is_loaded(self):
-        return self.is_displayed(self.HEADER)
